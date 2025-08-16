23e16a0770898b4674cf50f72b97e3e3233637f7
jihwan <jihwan0713@naver.com>
2025-08-16 14:28:19 +0900

    version.02

--- my_string.py
+++ my_string.py
@@ -1,10 +1,6 @@
-# 매개변수 string : my_string
-
-my_string = input()
-answer = []
-def string_ascending ():
-    answer = sorted([chr for chr in my_string if chr.isdigit()])
-    return answer
-    
-
-print(answer)
+def solution(my_string):
+    answer = []
+    for ch in my_string:
+        if ch.isdigit():
+            answer.append(int(ch))
+    return sorted(answer)
