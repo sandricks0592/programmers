2dd0e7c74dea8a3e92cee5c4fa65188bc327bf00
jihwan <jihwan0713@naver.com>
2025-08-20 14:33:41 +0900

    8/20

--- level00/tentative_scoring.py
+++ level00/tentative_scoring.py
@@ -5,7 +5,7 @@
 def solution(numbers, our_score, score_list):
     answer = []
     for i in range(len(numbers)):
-        if our_score[numbers[i]] == score_list[i]:
+        if our_score[i] == score_list[numbers[i]-1]:  
             answer.append("Same")
         else:
             answer.append("Different")
