b5173b1e4b8db5df2327cb3afe672c7929767bc6
jihwan <jihwan0713@naver.com>
2025-08-26 14:18:21 +0900

    8/26

--- basic_coding_training/all_suffix_print.py
+++ basic_coding_training/all_suffix_print.py
@@ -1,7 +1,9 @@
 def solution(my_string):
     answer = []
+    a = []
     for i in range(len(my_string)):
-        answer.append(my_string[i:])
+        a.append(my_string[i:])
+        answer = sorted(a)
     return answer
 
 print(solution("banana"))
