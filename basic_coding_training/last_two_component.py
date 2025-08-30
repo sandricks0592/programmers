40c013651c9ab27eb82a115b1047ef7d66c5a696
jihwan <jihwan0713@naver.com>
2025-08-30 14:40:04 +0900

    8/30

---
+++ basic_coding_training/last_two_component.py
@@ -0,0 +1,11 @@
+def solution(num_list):
+    answer = []
+    answer = num_list
+    if num_list[-1] > num_list[-2]:
+        answer.append(num_list[-1]-num_list[-2])
+    else:
+        answer.append(2*num_list[-1])
+    return answer
+num_list = [2, 1, 6]
+
+print(solution(num_list))
--- basic_coding_training/overwrite.py
+++ basic_coding_training/overwrite.py
@@ -2,3 +2,5 @@ def solution(my_string, overwrite_string, s):
     answer = ''
     answer = my_string[:s]+overwrite_string+my_string[s+len(overwrite_string):]
     return answer
+
+# 뒤 my_string삽입 조건을 좀 더 생각해보기
