c96212d180571c1b4ef94456c1d32863bd967320
jihwan <jihwan0713@naver.com>
2025-09-01 15:44:00 +0900

    9/1

---
+++ basic_coding_training/make_a_specified_character_uppercase.py
@@ -0,0 +1,18 @@
+# def solution(my_string, alp):
+#     answer = ''
+#     for i in range(len(my_string)):
+#         if my_string[i] == alp[0]:
+#             answer = my_string[:i]+my_string[i].upper()+my_string[i+1:]    
+#         else:
+#             answer = my_string       
+#     return answer
+#  위 코드는 하나만 대문자로 만들수 있다. 중복일 경우 불가능
+
+def solution(my_string, alp):
+    answer = ''
+    answer = my_string.replace(alp,alp.upper())
+    return answer
+
+
+print(solution("programmers","p"))
+
