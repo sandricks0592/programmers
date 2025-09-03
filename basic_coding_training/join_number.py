8ca5c49ffbdbe92c839c9f85e55e967d0fca6c48
jihwan <jihwan0713@naver.com>
2025-09-03 16:33:36 +0900

    9/3

---
+++ basic_coding_training/join_number.py
@@ -0,0 +1,28 @@
+# def solution(num_list):
+#     answer = 0
+#     odd = ''
+#     even = ''
+#     for i in range(0,len(num_list),2):
+#         even += str(num_list[i])
+#     for e in range(1,len(num_list),2):
+#         odd += str(num_list[e]) 
+#     answer = int(odd) + int(even)
+#     return answer
+
+
+def solution(num_list):
+    answer = 0
+    odd = ''
+    even = ''
+    for i in range(len(num_list)):
+        if num_list[i]%2 ==0:
+            even += str(num_list[i])
+        elif num_list[i]%2 ==1:
+            odd += str(num_list[i])
+    answer = int(odd) + int(even)
+    return answer
+
+
+num_list = [3,4,5,2,1]
+
+print(solution(num_list))
