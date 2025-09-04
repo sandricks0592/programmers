6e1f03fef96bff1f388638b6bb0d4310bd792166
jihwan <jihwan0713@naver.com>
2025-09-04 16:28:53 +0900

    9/4

---
+++ basic_coding_training/make_I.py
@@ -0,0 +1,18 @@
+# def solution(myString):
+#     answer = ''
+#     for i in range(len(myString)):
+#         if myString.upper()[i] <= 'I':
+#             answer = myString[:i]+'I'+myString[i+1:]
+#     return answer
+
+def solution(myString):
+    answer = ''
+    for ch in myString:
+        if ch.upper() <= "I":
+            answer += "l"
+        else:
+            answer += ch
+    return answer
+
+myString = "abcdevwxyz"
+print(solution(myString))
--- basic_coding_training/sequence_and_quert02.py
+++ basic_coding_training/sequence_and_quert02.py
@@ -1,7 +1,23 @@
+# def solution(arr, queries):
+#     answer = []    
+#     for s,e,k in queries:
+#         for i in range(len(arr)):
+#             if s <= arr[i] <= e and arr[i] >= k:
+#                 answer += min.arr[i]
+#     return answer
+
+
 def solution(arr, queries):
-    answer = []    
-    for s,e,k in queries:
-        for i in range(len(arr)):
-            if s <= arr[i] <= e and arr[i] >= k:
-                answer += min.arr[i]
+    answer = []
+    for s, e, k in queries:
+        # s <= i <= e 범위의 arr[i] 중 k보다 큰 값만 추출
+        candidates = [arr[i] for i in range(s, e+1) if arr[i] > k]
+        
+        if candidates:          # 후보가 있으면 최소값
+            answer.append(min(candidates))
+        else:                  # 없으면 -1
+            answer.append(-1)
+            
     return answer
+
+# 
