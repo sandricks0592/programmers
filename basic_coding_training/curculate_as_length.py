3f61599a1822ac0b0613ca6cd0b52a772100e7fb
jihwan <jihwan0713@naver.com>
2025-09-22 13:42:10 +0900

    9/22

--- basic_coding_training/curculate_as_length.py
+++ basic_coding_training/curculate_as_length.py
@@ -1,3 +1,13 @@
+# def solution(num_list):
+#     answer = 0
+#     if len(num_list) >= 11:
+#         for i in num_list:
+#             answer += i
+#     elif len(num_list) <= 10:
+#         for i in num_list:
+#             answer *= i
+#     return answer
+
 def solution(num_list):
     if len(num_list) >= 11:
         answer = 0
@@ -11,3 +21,5 @@ def solution(num_list):
 
 num_list = [2, 3, 4, 5]
 print(solution(num_list))
+
+#  answer 초기 값을 생각하기
---
+++ basic_coding_training/find_desired_string.py
@@ -0,0 +1,11 @@
+def solution(myString, pat):
+    answer = 0
+    if pat.upper() in myString.upper():
+        answer = 1
+    else:
+        answer = 0
+    return answer
+
+myString = "AbCdEfG"
+pat = "aBc"
+print(solution(myString, pat))
