ff4a9b088b2f5e24d5863d4361500ac2838a5f4f
jihwan <jihwan0713@naver.com>
2025-09-22 13:17:53 +0900

    9/22-version02

--- basic_coding_training/make_as_1.py
+++ basic_coding_training/make_as_1.py
@@ -2,15 +2,18 @@ def solution(num_list):
     answer = 0
     x = 0
     for i in range(len(num_list)):
-        while num_list[i] >= 1:
-            if num_list[i] % 2 == 0:
-                num_list[i] = num_list[i] // 2
+        while int(num_list[i]) >= 1:
+            if int(num_list[i]) % 2 == 0:
+                num_list[i]) = int(num_list[i]) // 2
                 x += 1
-            elif num_list[i] % 2 == 1:
-                num_list[i] = (num_list[i] - 1) // 2
+            elif int(num_list[i]) % 2 == 1:
+                int(num_list[i]) = (int(num_list[i]) - 1) // 2
                 x += 1
         answer = x
     return answer
 
 num_list = [12, 4, 15, 1, 14]
 print(solution(num_list))
+
+# 연산에서 합차가 먼저인지 곱 나누기가 먼저인지 생각해보기
+# range()를 이용하면 시간이 오버 할 수 있어서 바로 num_list 요소를 사용할 수 있게 해보자.
