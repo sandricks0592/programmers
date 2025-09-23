9a01d740ff8716f262410b2086d64c0be5bcc53e
jihwan <jihwan0713@naver.com>
2025-09-23 14:16:22 +0900

    9/23

--- basic_coding_training/separate_space.py
+++ basic_coding_training/separate_space.py
@@ -5,3 +5,5 @@ def solution(my_string):
 
 my_string = "i love you"
 print(solution(my_string))
+
+#  ''.join()은 하나의 문자열로 만들고 split()은 문자열을 리스트로 만듬
---
+++ basic_coding_training/separate_space01.py
@@ -0,0 +1,7 @@
+def solution(my_string):
+    answer = []
+    answer = my_string
+    return answer.split()
+
+my_string = "i love you"
+print(solution(my_string))
