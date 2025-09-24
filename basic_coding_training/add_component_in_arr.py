a25d104d884be2ebad1a5fb786898fd077df6496
jihwan <jihwan0713@naver.com>
2025-09-24 16:30:37 +0900

    9/24

--- basic_coding_training/add_component_in_arr.py
+++ basic_coding_training/add_component_in_arr.py
@@ -1,9 +1,18 @@
+# def solution(arr):
+#     answer = []
+#     for i in range(len(arr)):
+#         while arr[i] > 0:
+#             answer.append(arr[i])
+#     return answer
+
 def solution(arr):
     answer = []
-    for i in range(len(arr)):
-        while arr[i] > 0:
-            answer.append(arr[i])
+    for num in arr:
+        answer.extend([num] * num)
     return answer
+
 
 arr = [5, 1, 4]
 print(solution(arr))
+
+# append()는 리스트 끝에 x라는 하나의 요소를 추가하지만 extend()는 각각의 요소로 추가한다.
---
+++ basic_coding_training/empty_array_add,del.py
@@ -0,0 +1,25 @@
+# def solution(arr, flag):
+#     answer = []
+#     for v,k in zip(arr, flag):
+#         for i in range(len(flag)):
+#             if k[i] == True:
+#                 answer.extend([v]*2)
+#             else:
+#                 answer.pop(k[-1])
+#     return answer
+
+def solution(arr, flag):
+    answer = []
+    for v, k in zip(arr, flag):
+        if k:  # True일 때
+            answer.extend([v] * (v*2))
+        else:  # False일 때
+            for _ in range(v):
+                if answer:   # 리스트가 비어있지 않을 때만 제거
+                    answer.pop()
+    return answer
+
+
+arr = [3, 2, 4, 1, 3]
+flag = [True, False, True, False, False]
+print(solution(arr, flag))
