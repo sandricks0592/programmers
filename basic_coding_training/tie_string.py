607047ad6342408fd373d8307830b73aa3b2de83
jihwan <jihwan0713@naver.com>
2025-09-30 15:25:52 +0900

    9/30

---
+++ basic_coding_training/tie_string.py
@@ -0,0 +1,21 @@
+# def solution(strArr):
+#     answer = 0
+#     num = []
+#     for i in range(len(strArr)):
+#         if len(strArr[i]) != 0:
+#             num.append(len(strArr[i]))
+#     answer = max(num, key =num.count)
+    
+#     return num.count(answer)
+
+def solution(strArr):
+    answer = 0
+    length_count = {}
+    for i in strArr:
+        l = len(i)
+        length_count[l] = length_count.get(l, 0) +1
+    answer = max(length_count.values())
+    return answer
+
+strArr = ["a","bc","d","efg","hi"]
+print(solution(strArr))
