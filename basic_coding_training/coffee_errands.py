2a12fb4d792e583612a15361108a13fc08a54ca3
jihwan <jihwan0713@naver.com>
2025-10-01 17:52:35 +0900

    10/1

---
+++ basic_coding_training/coffee_errands.py
@@ -0,0 +1,11 @@
+def solution(order):
+    answer = 0
+    for i in order:
+        if 'cafelatte' in i:
+            answer += 5000
+        else:
+            answer += 4500
+    return answer
+
+order = ["cafelatte", "americanoice", "hotcafelatte", "anything"]
+print(solution(order))
--- basic_coding_training/compare_weather.py
+++ basic_coding_training/compare_weather.py
@@ -1,20 +1,24 @@
+# def solution(date1, date2):
+#     answer = 0
+#     if date1[0] < date2[0]:
+#         answer = 1
+#     elif date1[0] > date2[0]:
+#         answer = 0
+#     else:
+#         if date1[1] < date2[1]:
+#             answer = 1
+#         elif date1[1] > date2[1]:
+#             answer = 0
+#         else:
+#             if date1[2] < date2[2]:
+#                 answer = 1
+#             else:
+#                 answer = 0
+#     return answer
+
+
 def solution(date1, date2):
-    answer = 0
-    if date1[0] < date2[0]:
-        answer = 1
-    elif date1[0] > date2[0]:
-        answer = 0
-    else:
-        if date1[1] < date2[1]:
-            answer = 1
-        elif date1[1] > date2[1]:
-            answer = 0
-        else:
-            if date1[2] < date2[2]:
-                answer = 1
-            else:
-                answer = 0
-    return answer
+    return int(date1 < date2)
 
 date1 = [2021, 12, 28]	
 date2 = [2021, 12, 29]
