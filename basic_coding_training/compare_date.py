c58d7d8e5ddaadb0982a43e94de5942eaaf68dc2
jihwan <jihwan0713@naver.com>
2025-10-01 17:44:26 +0900

    10/1version02

---
+++ basic_coding_training/compare_date.py
@@ -0,0 +1,27 @@
+# def solution(date1, date2):
+#     answer = 0
+#     if date1[0] < date2[0]:
+#         answer = 1
+#     elif date1[0] > date2[0]:
+#         answer = 0
+#     else:
+#         if date1[1] < date2[1]:
+#             answer = 1
+#         elif date1[1] > date2[1]:
+#             answer = 0
+#         else:
+#             if date1[2] < date2[2]:
+#                 answer = 1
+#             else:
+#                 answer = 0
+#     return answer
+
+
+2
+3
+def solution(date1, date2):
+    return int(date1 , date2)
+
+date1 = [2021, 12, 28]	
+date2 = [2021, 12, 29]
+print(solution(date1, date2))
