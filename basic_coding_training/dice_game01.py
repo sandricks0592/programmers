d7241d9d0438e5e3c49d913b3f968e3171a96693
jihwan <jihwan0713@naver.com>
2025-10-01 16:37:32 +0900

    10/1

---
+++ basic_coding_training/dice_game01.py
@@ -0,0 +1,12 @@
+def solution(a, b):
+    answer = 0
+    if a%2==1 and b%2 == 1:
+        answer = a ** 2 + b ** 2
+    elif a%2 ==0 and b%2 == 0:
+        answer = abs(a - b )
+    else:
+        answer = 2 * (a + b)
+    return answer
+a = 3
+b = 5
+print(solution(a, b))
