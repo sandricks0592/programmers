5dea2fd1569b25e0edfb1ae0da85c2f6f183655b
jihwan <jihwan0713@naver.com>
2025-10-02 11:44:52 +0900

    10/2

---
+++ basic_coding_training/make_square.py
@@ -0,0 +1,38 @@
+# def solution(arr):
+#     answer = [[]]
+#     for r in arr:
+#         for l in r:
+#             if len(r) > len(l):
+#                 while len(r) == len(l):
+#                     l.append("0")
+#             elif len(l) > len(r):
+#                 while len(r) == len(l):
+#                     r.append("0")
+#             else:
+#                 answer = arr
+#         answer = arr
+#     return answer
+
+def solution(arr):
+    rows = len(arr)            # 행의 수
+    cols = max(len(r) for r in arr)  # 가장 긴 열의 길이
+
+    # 목표 크기 = 행과 열 중 큰 값
+    n = max(rows, cols)
+
+    # 모든 행의 길이를 n으로 맞추기
+    for row in arr:
+        while len(row) < n:
+            row.append(0)
+
+    # 행의 개수를 n으로 맞추기
+    while len(arr) < n:
+        arr.append([0] * n)
+
+    return arr
+
+
+arr = [[572, 22, 37], [287, 726, 384], [85, 137, 292], [487, 13, 876]]
+print(solution(arr))
+
+# 
--- basic_coding_training/painting_enlargement.py
+++ basic_coding_training/painting_enlargement.py
@@ -5,17 +5,29 @@
 #             answer.append(str(i[j])*k)
 #     return answer
 
-def solution(picture,k):
+def solution(picture, k):
     answer = []
+
     for row in picture:
-        new_row = ''
-        for ch in row:
-            new_row += ch * k
-            for _ in range(k):
-                answer.append(new_row)
+        new_row = ""              # 한 행 전체를 담을 변수
+        for ch in row:            # row 안의 문자 하나씩
+            new_row += ch * k     # 문자 수평 확대
+
+        for _ in range(k):        # 수직 확대
+            answer.append(new_row)
 
     return answer
+
+
+# def solution(picture, k):
+#     answer = []
+#     for i in range(len(picture)):
+#         for _ in range(k):
+#             answer.append(picture[i].replace('.', '.' * k).replace('x', 'x' * k))
+#     return answer
 
 picture = [".xx...xx.", "x..x.x..x", "x...x...x", ".x.....x.", "..x...x..", "...x.x...", "....x...."]
 k = 2
 print(solution(picture, k))
+
+# for 문 여러개로 string을 분리 가능하다. 가로 세로 잘 구별해서 사용하기, replace도 사용 가능하다.
