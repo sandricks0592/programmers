638426d3e0a20516b370f56c5cc2782fc2f9e5e8
jihwan <jihwan0713@naver.com>
2025-10-28 15:59:33 +0900

    10/28

---
+++ beginner_coding_training/count_K.py
@@ -0,0 +1,14 @@
+def solution(i, j, k):
+    answer = 0
+    str_k = str(k)
+    for a in range(i,j+1):
+        if str_k in str(a): 
+            answer += str(a).count(str_k)
+    return answer
+
+i = 1
+j = 13
+k = 1
+print(solution(i, j, k))
+
+# 
