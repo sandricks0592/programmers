6eb085b25febfe794cf687f042984868fd989ce1
jihwan <jihwan0713@naver.com>
2025-11-19 16:46:40 +0900

    11/19

---
+++ beginner_coding_training/push_str.py
@@ -0,0 +1,13 @@
+def solution(A, B):
+    answer = 0
+    for i in range(len(A)):
+        if A == B:
+            return i
+        A = A[-1]+A[:-1]
+    
+    
+    return -1
+
+A = "hello"
+B = "ohell"	
+print(solution(A, B))
