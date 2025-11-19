c2b9d43a56d84a84b712266b39cb8fa92edf7c33
jihwan <jihwan0713@naver.com>
2025-11-19 15:41:58 +0900

    11/19

--- beginner_coding_training/find_composite_number.py
+++ beginner_coding_training/find_composite_number.py
@@ -1,8 +1,15 @@
 def solution(n):
     answer = 0
-     if n <= 3:
-       
+    for num in range(1,n+1):
+        divisor = 0
+        for i in range(1,num+1):
+            if num % i == 0:
+                divisor += 1
+        if divisor >= 3:
+            answer += 1
     return answer
 
 n = 10
 print(solution(n))
+
+# 이중 for을 사용하여 숫자 하나씩의 약수의 갯수를 분석한다.
