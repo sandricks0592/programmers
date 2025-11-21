d907db47ab739ac6016eef538bf8112ceefd2db9
jihwan <jihwan0713@naver.com>
2025-11-21 12:01:26 +0900

    11/21

---
+++ beginner_coding_training/ctr_z.py
@@ -0,0 +1,12 @@
+def solution(s):
+    stack = []
+    for i in s.split():
+        if i == 'Z':
+            if stack:
+                stack.pop()
+        else:
+            stack.append(int(i))
+    return sum(stack)
+
+s = "1 2 Z 3"
+print(solution(s))
