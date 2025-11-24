25f49179855a5cff85b6ad90e25e60ce7152e861
jihwan <jihwan0713@naver.com>
2025-11-24 15:33:58 +0900

    다시보기

--- beginner_coding_training/hidden_num_add02.py
+++ beginner_coding_training/hidden_num_add02.py
@@ -1,12 +1,25 @@
-def solution(my_string):
-    answer = 0
-    ch_num = ''
-    for i in my_string:
-        if i.isdigit():
-            answer += int(i)
-        else:
+# def solution(my_string):
+#     answer = 0
+#     ch_num = ''
+#     for i in my_string:
+#         if i.isdigit():
+#             answer += int(i)
+#         else:
             
-    return answer
+#     return answer
 
-my_string = "aAb1B2cC34oOp"
+# my_string = "aAb1B2cC34oOp"
+# print(solution(my_string))
+
+def solution(my_string):
+    for c in my_string:
+        if not c.isdigit():   # 숫자가 아니면
+            my_string = my_string.replace(c, ' ')
+    nums = my_string.split()
+    
+    return sum(map(int, nums)) if nums else 0
+
+my_string = "aAb1B2cC34oOp"	
 print(solution(my_string))
+
+# 문자들은 공백으로 replace한 다음 split하여 숫자들을 빼준다음 int형으로 만들어 다 더해준다ㅑ.
