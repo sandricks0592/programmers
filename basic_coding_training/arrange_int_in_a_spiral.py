8e0516ba0375273cded3434df8f23501672b0a92
jihwan <jihwan0713@naver.com>
2025-12-31 11:04:46 +0900

    final

--- basic_coding_training/arrange_int_in_a_spiral.py
+++ basic_coding_training/arrange_int_in_a_spiral.py
@@ -1,12 +1,23 @@
-def solution(n):
-    answer = []
-    basic = [[0]*n for _ in range(n)]
+# def solution(n):
+#     answer = []
+#     basic = [[0]*n for _ in range(n)]
 
-    for i in range(basic[0][0],basic[0][n-1]):
-        i += 1
+#     for i in range(basic[0][0],basic[0][n-1]):
+#         i += 1
     
 
-    return basic
+#     return basic
+
+def solution(n):
+    answer = [[None for j in range(n)] for i in range(n)]
+    move = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+    x, y, m = 0, 0, 0
+    for i in range(1, n**2 + 1):
+        answer[y][x] = i
+        if y + move[m][0] >= n or x + move[m][1] >= n or answer[y + move[m][0]][x + move[m][1]]:
+            m = (m + 1) % len(move)
+        y, x = y + move[m][0], x + move[m][1]
+    return answer
 
 n = 4
 print(solution(n))
